ff135266a23d3b898bb37029cfe87df02cd4cf37
dxnvrs <mt.luz14@gmail.com>
2026-02-22 17:26:34 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,7 +9,11 @@ def main():
 
         # waiting for user's input
         command = input()
+        # if the user types "exit", break the loop and end the program
+        if command == "exit":
+            break
         print(f"{command}: command not found")
 
+        
 if __name__ == "__main__":
     main()
