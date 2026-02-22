3d43d84f96f0cedfda2201b01b50bd2dd56a3a85
dxnvrs <mt.luz14@gmail.com>
2026-02-22 18:45:54 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -3,13 +3,16 @@ import os
 
 
 def main():
-    rcvPATH = os.environ.get('PATH')
-    dirs = rcvPATH.split(os.pathsep)
+    
 
     # TODO: Uncomment the code below to pass the first stage
     while True:
+        rcvPATH = os.environ.get('PATH')
+        dirs = rcvPATH.split(os.pathsep)
+        
         sys.stdout.write("$ ")
         BUILTIN = ['echo', 'exit', 'type']
+        
         # waiting for user's input
         command = input()
        
