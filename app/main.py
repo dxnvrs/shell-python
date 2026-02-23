3f0ce53545d143d8a8465d9353ce6804a71db172
dxnvrs <mt.luz14@gmail.com>
2026-02-23 00:13:28 -0300

    [any message]

--- app/main.py
+++ app/main.py
@@ -19,34 +19,30 @@ def main():
         command = input()
         # split the command into the command name and its arguments using shlex.split to handle quoted strings properly
         parts = shlex.split(command)  
+        
+        outputFile = None
+
+        for i in ['>', '1>']:
+            if i in parts:
+                index = parts.index(i)
+                outputFile = parts[index + 1]
+                parts = parts[:index]  + parts[index+2] # Remove the redirection part from the command
+                break
+        if not parts: continue
+        
         cmdName = parts[0]
         args = parts[1:]
 
-        if '>' in args or '1>' in args:
-            # Handle output redirection
-            output_file = None
-            if '>' in args:
-                output_file = args[args.index('>') + 1]
-            elif '1>' in args:
-                output_file = args[args.index('1>') + 1]
-            
-            # Remove redirection arguments from args list
-            filtered_args = []
-            skip_next = False
-            for arg in args:
-                if skip_next:
-                    skip_next = False
-                    continue
-                if arg == '>' or arg == '1>':
-                    skip_next = True
-                    continue
-                filtered_args.append(arg)
-
-            with open(output_file, 'w') as f:
-                subprocess.run([cmdName] + filtered_args, stdout=f, stderr=f)
+        def shellPrint(content):
+            if outputFile:
+                with open(outputFile, 'w') as f:
+                    f.write(content + '\n')
+            else:
+                print(content)
+        
        
         if cmdName == 'echo':
-            print(" ".join(args))
+            shellPrint(" ".join(args))
          # if the user types "exit", break the loop and end the program
         elif cmdName == 'exit':
             if args and args[0] == '0':
@@ -56,27 +52,27 @@ def main():
         elif cmdName == 'type':
             target = args[0]
             if target in BUILTIN:
-                print(f"{target} is a shell builtin")
+                shellPrint(f"{target} is a shell builtin")
             else:
                 found = False
                 for directory in dirs:
                     full_path = os.path.join(directory, target)
                     if os.path.exists(full_path) and os.access(full_path, os.X_OK):
-                        print(f"{target} is {full_path}")
+                        shellPrint(f"{target} is {full_path}")
                         found = True
                         break
                 if not found:
-                    print(f"{target}: not found")
+                    shellPrint(f"{target}: not found")
         # if the user types "pwd", print the current working directory
         elif cmdName == 'pwd':
-            print(os.getcwd())
+            shellPrint(os.getcwd())
         # if the user types "cd <directory>", change the current working directory to the specified directory. If no directory is specified, change to the user's home directory. If the specified directory does not exist, print an error message
         elif cmdName == 'cd':
             if args and args[0] != '~':
                 try:
                     os.chdir(args[0])
                 except FileNotFoundError:
-                    print(f"cd: {args[0]}: No such file or directory")
+                    shellPrint(f"cd: {args[0]}: No such file or directory")
             else:
                 # If no directory is specified or the argument is '~', change to the user's home directory
                 os.chdir(os.path.expanduser('~'))
@@ -94,7 +90,6 @@ def main():
                 subprocess.run([cmdName] + args, executable=foundPath)
             else:
                 print(f"{cmdName}: command not found")
-
-        
+       
 if __name__ == "__main__":
     main()
